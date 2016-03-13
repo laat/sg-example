d9f57cdbb48e3c293a44408ec959e1cad25000d5
Sigurd Fosseng <sigurd@fosseng.net>
2016-03-13 17:05:07 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,29 @@
+# sg-example
+
+A web component for displaying code examples in styleguides.
+
+## Usage
+
+```html
+<style>
+    nav {
+        border: 1px solid #ddd;
+    }
+    li {
+        background-color: #fff;
+        color: #f43;
+    }
+</style>
+<sg-example>
+    <nav>
+        <ul>
+            <li>example element 1</li>
+            <li>example element 2</li>
+        </ul>
+    </nav>
+</sg-example>
+```
+
+Gives this output:
+
+![example](demo/demo.png)
---
+++ src/sg-example.js
@@ -0,0 +1,41 @@
+import unindent from './unindent'
+import hljs from 'highlight.js'
+
+let owner
+if (window.HTMLImports && !window.HTMLImports.useNative) {
+  owner = document._currentScript.ownerDocument
+} else {
+  owner = document.currentScript.ownerDocument
+}
+
+const codeExample = owner.querySelector('#sgCodeExample')
+function createCodeExample (code) {
+  const highlighted = hljs.highlight('html', code).value
+  const c = codeExample.content.cloneNode(true)
+  c.querySelector('code').innerHTML = highlighted
+
+  const input = c.querySelector('.sg-copy-data')
+  input.value = code
+
+  const copyButton = c.querySelector('.sg-copy-button')
+  copyButton.addEventListener('click', (evt) => {
+    input.select()
+    document.execCommand('copy')
+  })
+  return c
+}
+
+class SgExample extends window.HTMLElement {
+  createdCallback () {
+    let code = this.innerHTML
+          .split(/\r?\n/)
+          .filter(line => line.trim() !== '')
+          .map(unindent())
+          .join('\n')
+
+    this._root = this.createShadowRoot()
+    this._root.appendChild(createCodeExample(code))
+  }
+}
+
+export default document.registerElement('sg-example', SgExample)
